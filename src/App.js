a9aabf649edabf9db8a7e921b7a15841952dda06
Regisl33 <reg33gaming@gmail.com>
2023-12-05 22:49:08 -0500

    project finished

--- src/App.js
+++ src/App.js
@@ -4,20 +4,29 @@ import Card from "./Card";
 
 const App = () => {
   const [data, setData] = useState([]);
-  useEffect(() => {
+  const [value, setValue] = useState([""]);
+  const fetch = (variable) => {
     axios
-      .get("https://www.themealdb.com/api/json/v1/1/search.php?s=Tomato")
+      .get("https://www.themealdb.com/api/json/v1/1/search.php?s=" + variable)
       .then((res) => setData(res.data.meals));
-  }, []);
+  };
+  const handleInput = (e) => {
+    setValue(e.target.value);
+    fetch(value);
+  };
+
+  useEffect(() => fetch(value), []);
 
   return (
     <main className="main">
       <h1>React Cook</h1>
-      <input type="text" placeholder="Tapez le nom d'un aliment (en anglais)" />
+      <input
+        type="text"
+        placeholder="Tapez le nom d'un aliment (en anglais)"
+        onChange={(e) => handleInput(e)}
+      />
       <div className="meals">
-        {data.map((meal, index) => (
-          <Card key={index} meal={meal} />
-        ))}
+        {data && data.map((meal, index) => <Card key={index} meal={meal} />)}
       </div>
     </main>
   );
